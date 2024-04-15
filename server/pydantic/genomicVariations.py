c72870caf594b2a172a917c517a02b2514c864e9
Oriol López-Doriga <orildsaga@gmail.com>
2024-04-15 12:35:54 +0200

    MT added for chr validated chromosomes

--- server/pydantic/genomicVariations.py
+++ server/pydantic/genomicVariations.py
@@ -173,7 +173,7 @@ class ChromosomeLocation(BaseModel):
     @field_validator('chr')
     @classmethod
     def chr_allowed(cls, v: str) -> str:
-        list_chr= ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","X","Y"]
+        list_chr= ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","X","Y", "MT"]
         if v in list_chr:
             pass
         else:
